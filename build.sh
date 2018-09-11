f9d5804645a7eb43f4177f829f9f27d3754dfd8b
christopherstanfield <43140237+christopherstanfield@users.noreply.github.com>
2018-09-11 09:25:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "test update and build via scm hook"
+echo "test another update and build via scm hook"
 for (( c=1; c<=5; c++ ))
 do  
    echo "Welcome $c  $BUILD_NUMBER  $name to the Jenkins class"
